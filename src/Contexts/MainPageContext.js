f9878ea83d1f6c7f7241840a43703369610082d1
Jose <jozylopez27@gmail.com>
2020-04-22 11:40:31 -0500

    profile component update

--- src/Contexts/MainPageContext.js
+++ src/Contexts/MainPageContext.js
@@ -4,8 +4,11 @@ const MainPageContext = React.createContext({
     users: [],
     firstUser: {},
     state: {},
+    isEditing: false,
     setUsers: () => {},
     setCurrentProfile: () => {},
+    setEditingToTrue: () => {},
+    resetEditing: () => {},
 })
 
 export default MainPageContext
@@ -14,6 +17,7 @@ export class MainPageProvider extends React.Component {
     state = {
         users: [],
         firstUser: {},
+        isEditing: false,
     }
 
     setUsers = users => {
@@ -24,13 +28,24 @@ export class MainPageProvider extends React.Component {
         this.setState({ firstUser: user })
     }
 
+    setEditingToTrue = () => {
+        this.setState({ isEditing: true })
+    }
+
+    resetEditing = () => {
+        this.setState({ isEditing: false })
+    }
+
     render() {
         const value = {
             users: this.state.users,
             firstUser: this.state.firstUser,
             state: this.state,
             setUsers: this.setUsers,
-            setCurrentProfile: this.setCurrentProfile
+            setCurrentProfile: this.setCurrentProfile,
+            isEditing: this.state.isEditing,
+            setEditingToTrue: this.setEditingToTrue,
+            resetEditing: this.resetEditing,
         }
 
         return (
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,22 +2,73 @@ import React from 'react'
 import './Profile.css'
 import { Link } from 'react-router-dom'
 import users from '../../store'
+import MainPageContext from '../../Contexts/MainPageContext'
 
 export default class Profile extends React.Component {
+    static contextType = MainPageContext
+
+    componentDidMount() {
+        this.context.resetEditing()
+    }
+
+    handleEditButton = event => {
+    event.preventDefault()
+
+    this.context.setEditingToTrue()
+    }
+
+    saveEdit = event => {
+        event.preventDefault()
+
+        this.resetEditing()
+    }
+
+    componentWillUnmount() {
+        this.context.resetEditing()
+    }
+
     render() {
         const currentUser = users[2] || {}
+
+        if(!this.context.isEditing) {
         return (
             <>
             <h3 className='profileH3'>My Profile</h3>
             <Link to='/swipe' className='profileLink'>Main</Link>
             {<div className='editDiv'> 
-            <img src={currentUser.avatar} alt='avatar' className='profileImg'></img>
+            <img src={currentUser.avatar} 
+            alt='avatar' className='profileImg'></img>
             <h4>Username: {currentUser.display_name}</h4>
             <p>Bio: {currentUser.bio}</p>
             <span> Platforms: {currentUser.platforms}</span>
             <span> lfm in: {currentUser.lfm_in}</span>
             </div>}
+            <button className='editButton' 
+            onClick={this.handleEditButton}>edit</button>
             </>
         )
+        } else if(this.context.isEditing) {
+            return (
+                <>
+                <img src={currentUser.avatar} 
+                alt='avatar' className='profileImgEdit'></img>
+                <form className='editForm'>
+                    <label htmlFor='username'>Username </label>
+                    <input type='text' name='username' 
+                    id='username' defaultValue={currentUser.display_name}></input>
+                    <label htmlFor='bio'>Bio </label>
+                    <input type='text' name='bio' 
+                    id='bio' defaultValue={currentUser.bio}></input>
+                    <label htmlFor='lfm'>lfm in </label>
+                    <input type='text' name='lfm' 
+                    id='lfm' defaultValue={currentUser.lfm_in}></input>
+                    <label htmlFor='platforms'> Platforms: </label>
+                    <input type='text' name='platforms' 
+                    id='platforms' defaultValue={currentUser.platforms}></input>
+                    <input type='submit' onSubmit={this.saveEdit}></input>
+                </form>
+                </>
+            )
+        }
     }
 }
