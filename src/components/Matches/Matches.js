b73c0fc510c3ae810b060758988d5be9a74413cb
gage117 <57643501+gage117@users.noreply.github.com>
2020-04-24 13:51:02 -0500

    Started matches styling

--- src/components/Matches/Matches.js
+++ src/components/Matches/Matches.js
@@ -2,18 +2,30 @@ import React from 'react'
 import './Matches.css'
 import users from '../../store'
 import {Link } from 'react-router-dom'
+import cardsIcon from '../../images/cards.png'
+// import nintendoNetworkLogo from '../../images/nintendo_logo.png'
+// import playstationLogo from '../../images/Playstation_logo_colour.svg'
+// import PC_Logo from '../../images/PC_Keyboard_Mouse_Icon.png'
+// import xboxLogo from '../../images/Xbox_one_logo.svg'
+
 
 export default class Matches extends React.Component {
 
     render() {
         return (
             <>
-            <h3 className='matchH3'>Matches</h3>
-            <Link to='/swipe' className=''>Main</Link>
-            <ul className='matchUl'>
-                {users.map(user => <li key={user.user_id}>
-                <img src={user.avatar} alt='avatar' className='matchImg'></img>
-                <h4>{user.display_name}</h4>
+            <div className='matches__icons-container'>
+                <Link to='/swipe' className='matches__Link'>
+                    <img className='matches__cards-icon' src={cardsIcon} alt='swipe-cards-icon' />
+                </Link>
+            </div>
+            <header className='matches__header-container'>
+                <h3 className='matches__header'>Matches</h3>
+            </header>
+            <ul className='matches__ul'>
+                {users.map(user => <li key={user.user_id} className='match__li'>
+                <h4 className='match__display-name'>{user.display_name}</h4>
+                <img src={user.avatar} alt='avatar' className='match__avatar'></img>
                 </li>)}
             </ul>
             </>
