f0db1b084d6e8cb195269a056b99a9a6eb6c60a0
gage117 <57643501+gage117@users.noreply.github.com>
2020-05-02 14:43:26 -0500

    Hardcoded Heroku API endpoint. Was causing issues with Zeit deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://project-manhattan.herokuapp.com/api',
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'super-crazy-secret'
 }
