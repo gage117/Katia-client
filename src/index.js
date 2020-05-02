ac2a2821f7f6d9fbf3c5f849eb2fc8de7e1a39e6
gage117 <57643501+gage117@users.noreply.github.com>
2020-05-01 21:53:39 -0500

    Deleted ThemeContext attempt from themes branch

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import { UserProvider } from './Contexts/UserContext';
-import { ThemeProvider } from './Contexts/ThemeContext';
 
 ReactDOM.render(
   <BrowserRouter>
     <UserProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <App />
     </UserProvider>
   </BrowserRouter>,
   document.getElementById('root')
