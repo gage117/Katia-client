9e4a17c1f5eca82fdcacd53b853811c7096ba98b
Matthew Wagaman <averaqedev@gmail.com>
2020-05-05 13:17:44 -0700

    implement usage of arrow keys to swipe

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -37,6 +37,24 @@ export default class MainPage extends React.Component {
                 this.setState({ queue });
             })
             .catch(error => this.setState({error: error.message}));
+
+        document.addEventListener('keydown', (e) => {
+            if(e.keyCode === 37) {
+                this.swipeLeft();
+            } else if(e.keyCode === 39) {
+                this.swipeRight();
+            }
+        }, false);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', (e) => {
+            if(e.keyCode === 37) {
+                this.swipeLeft();
+            } else if(e.keyCode === 39) {
+                this.swipeRight();
+            }
+        }, false);
     }
 
     toggleExpanded = () => {
@@ -46,13 +64,15 @@ export default class MainPage extends React.Component {
     swipeLeft = () => {
         const { queue } = this.state;
         
-        const rejection = queue.dequeue()
+        const rejection = queue.dequeue();
 
-        SwipeService.addRejection(this.context.user_id, rejection.id)
-        .then(() => {
-            this.setState({ queue, expanded: false })
-        })
-        .catch(error => this.setState({ error }))
+        if(rejection.id) {
+            SwipeService.addRejection(this.context.user_id, rejection.id)
+                .then(() => {
+                    this.setState({ queue, expanded: false })
+                })
+                .catch(error => this.setState({ error }))
+        }
     }
 
     swipeRight = () => {
@@ -60,47 +80,14 @@ export default class MainPage extends React.Component {
 
         const match = queue.dequeue();
 
-        SwipeService.addMatch(this.context.user_id, match.id)
-            .then(() => {
-                this.setState({ queue, expanded: false });
-            })
-            .catch(error => this.setState({error}));
+        if(match.id) {
+            SwipeService.addMatch(this.context.user_id, match.id)
+                .then(() => {
+                    this.setState({ queue, expanded: false });
+                })
+                .catch(error => this.setState({error}));
+        }
     }
-
-    // generateUserCard = (user) => {
-    //     return (
-    //         <>
-    //         <li className='main__Swipe-User' onClick={this.toggleExpanded} >
-    //             {this.state.expanded ? (<></>) : (<img src={user.avatar} alt='avatar' className='main__Image' />)}
-    //             <h3 className='main__display-name'>{user.display_name}</h3>
-    //             <h4 className='main__card-header'>Platforms</h4>
-    //             <div className='main__platforms'>
-    //                 {user.platforms.includes("Xbox") ? <img className='main__xbox' src={xboxLogo} alt='Xbox logo' /> : null}
-    //                 {user.platforms.includes("PlayStation") ? <img className='main__playstation' src={playstationLogo} alt='Playstation logo' /> : null}
-    //                 {user.platforms.includes("Nintendo") ? <img className='main__nintendo' src={nintendoLogo} alt='Nintendo logo' /> : null}
-    //                 {user.platforms.includes("PC") ? <img className='main__PC' src={PC_Logo} alt='PC logo' /> : null}
-    //             </div>
-    //             <h4 className='main__card-header'>LFM In</h4>
-
-    //             {this.context.generateLfmElements(user.lfm_in)}
-
-    //             <h4 className='main__card-header'>Genres</h4>
-    //             <span>{this.context.generateGenreString(user.genres)}</span>
-    //             {this.state.expanded ? (<><h4 className='main__card-header'>Bio</h4>
-    //             <p className='main__bio'>{user.bio}</p></>)
-    //             :
-    //             (<></>)}
-    //             <div className='main__caret-container'>
-    //                 <input className={`main__down-caret${this.state.expanded ? ' reverse' : ''}`} type="image" src={down_caretSVG} alt='down-caret' />
-    //             </div>
-    //         </li>
-    //         <div className='main__Second-Nav'>
-    //             <img className='main__x' src={x_markSVG} alt='x' onClick={this.swipeLeft} />
-    //             <img className='main__check' src={checkmarkSVG} alt='checkmark' onClick={this.swipeRight} />
-    //         </div>
-    //         </>
-    //     )
-    // }
 
     generateUserCard = (user) => {
         const handlers = {
@@ -160,7 +147,7 @@ export default class MainPage extends React.Component {
 
         if(queue == null) {
             return (
-                <div className='loading'>loading</div>
+                <div className='loading'></div>
             )
         }
 
