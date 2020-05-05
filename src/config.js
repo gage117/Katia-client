f74bd9094438717e6305d6031bc8206a0756f205
Jose <jozylopez27@gmail.com>
2020-05-05 12:20:54 -0500

    updated endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://project-manhattan.herokuapp.com/api',
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'super-crazy-secret'
 }
+
+// https://project-manhattan.herokuapp.com/api
+// http://localhost:8000/api
