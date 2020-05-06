6ae0df0b1aa40f0fb86a30eba779c1446bf89a65
Ron Martin <ronmartin89@gmail.com>
2020-05-06 15:35:18 -0700

    formatted fixed for gamer tags

--- src/components/GamerTag/GamerTag.js
+++ src/components/GamerTag/GamerTag.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './GamerTag.css'
 
 export default function GamerTag(props) {
+
+    let formattedInput = props.name.charAt(0).toUpperCase() + props.name.slice(1);
     return(
       <>
-        <label htmlFor={props.name}>{props.name}</label>
+        <label htmlFor={props.name}>{props.name === 'psn' ? props.name.toUpperCase() : formattedInput}: </label>
         <input 
             type='text'
             name={props.name}
