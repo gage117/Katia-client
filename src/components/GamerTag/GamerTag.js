51d51173e637d30a4b068020f491ddc7365fb4bb
Ron Martin <ronmartin89@gmail.com>
2020-05-06 16:55:53 -0700

    updated styling and classnames

--- src/components/GamerTag/GamerTag.js
+++ src/components/GamerTag/GamerTag.js
@@ -6,9 +6,13 @@ export default function GamerTag(props) {
     let formattedInput = props.name.charAt(0).toUpperCase() + props.name.slice(1);
     return(
       <>
-        <label htmlFor={props.name}>{props.name === 'psn' ? props.name.toUpperCase() : formattedInput}: </label>
+        <label 
+          htmlFor={props.name}
+          className='gamer-tag__label'>
+            {props.name === 'psn' ? props.name.toUpperCase() : formattedInput}: </label>
         <input 
             type='text'
+            className='gamer-tag__input'
             name={props.name}
             onChange={e => props.handleChange(e)}
             id={props.name}
